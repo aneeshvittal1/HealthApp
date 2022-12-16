c62264899608c660984a011969a4f362c04dca22
Aneesh Vittal <akv6zr@virginia.edu>
2022-12-16 17:17:17 -0500

    Initial Commit

---
+++ CommunityApp/CommunityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityAppApp.swift
+//  CommunityApp
+//
+//  Created by Aneesh Vittal on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CommunityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
