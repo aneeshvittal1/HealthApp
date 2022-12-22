010f0aa73458f6f2722447586792f238f928a482
Aneesh Vittal <akv6zr@virginia.edu>
2022-12-22 02:08:34 -0500

    Buttons and search feature for map

--- CommunityApp/HomeView.swift
+++ CommunityApp/HomeView.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+
+struct MenuButton: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: 160, maxHeight: 50)
+            .background(Color(red: 0.90, green: 0.90, blue: 0.90))
+            .foregroundColor(.black)
+            .clipShape(Capsule())
+    }
+}
+
+
+
 struct HomeView: View {
     var body: some View {
         ScrollView{
@@ -26,15 +39,45 @@ struct HomeView: View {
                 ZStack{
                     RoundedRectangle(cornerRadius: 25)
                         .stroke(Color.black, lineWidth: 1)
-                        .shadow(color: .blue, radius: 2, x: 0, y: 4)
-                        .frame(width: 360, height: 300)
+                        .shadow(color: .black, radius: 2, x: 0, y: 4)
+                        .frame(width: 360, height: 265)
                         .opacity(0.3)
                     
                     RoundedRectangle(cornerRadius: 25)
                         .fill(Color.white)
-                        .frame(width: 360, height: 300)
+                        .frame(width: 360, height: 265)
                     
+                    HStack{
+                        VStack(spacing: 25){ //Left Column
+                            Button("Welfare Aid"){
+                                print("Button1")
+                            } .buttonStyle(MenuButton())
+                            
+                            Button("Transportation"){
+                                print("Button2")
+                            } .buttonStyle(MenuButton())
+                            
+                            Button("Substance Abuse"){
+                                print("Button3")
+                            } .buttonStyle(MenuButton())
+                        }
+                        
+                        VStack(spacing: 25){ //Right Column
+                            Button("Mental Health"){
+                                print("Button4")
+                            } .buttonStyle(MenuButton())
+                            
+                            Button("Hotlines"){
+                                print("Button5")
+                            } .buttonStyle(MenuButton())
+                            
+                            Button("Button 6"){
+                                print("Button6")
+                            } .buttonStyle(MenuButton())
+                        }
+                    }
                 }
+                
                 
                 Spacer()
                 Spacer()
@@ -59,10 +102,9 @@ struct HomeView: View {
                 
             }
         }
-        
+    }
         
     }
-}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
--- CommunityApp/MapView.swift
+++ CommunityApp/MapView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 38.845765, longitude: -77.259021), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 38.845861, longitude: -77.306246), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    
+    @State private var searchQuery = ""
     
     var body: some View {
-        VStack{
-            Map(coordinateRegion: $region)
-                .frame(maxWidth: 450, maxHeight: 550)
+        //VStack(){
+        NavigationStack(){
+            VStack(spacing: 10){
+                Map(coordinateRegion: $region)
+                    .padding(.top, -160.0)
+                    .frame(maxWidth: 450, maxHeight: 350)
+                Text("Searching for \(searchQuery)")
+                
+            }.searchable(text: $searchQuery)
         }
+        //}
     }
 }
 
